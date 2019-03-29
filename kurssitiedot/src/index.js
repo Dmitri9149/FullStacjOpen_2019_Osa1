458c5aff6916d187c8bc70d000499c73aedbe2e0
Dmitri <34885758+Dmitri9149@users.noreply.github.com>
2019-03-29 16:11:05 +0200

    1.4.kurssitiedot finished

--- kurssitiedot/src/index.js
+++ kurssitiedot/src/index.js
@@ -4,32 +4,33 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
   const course = 'Half Stack -sovelluskehitys'
-  const part1 = {
-    name: 'Reactin perusteet',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Tiedonvälitys propseilla',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'Komponenttien tila',
-    exercises: 14
-  }
-
+  const parts = [
+    {
+      name: 'Reactin perusteet',
+      exercises: 10
+    },
+    {
+      name: 'Tiedonvälitys propseilla',
+      exercises: 7
+    },
+    {
+      name: 'Komponenttien tila',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
       <Content 
-        part1 = {part1.name}  exercises1 = {part1.exercises}
-        part2 = {part2.name}  exercises2 = {part2.exercises}
-        part3 = {part3.name}  exercises3 = {part3.exercises}
+        part1 = {parts[0].name}  exercises1 = {parts[0].exercises}
+        part2 = {parts[1].name}  exercises2 = {parts[1].exercises}
+        part3 = {parts[2].name}  exercises3 = {parts[2].exercises}
        />
       
-      <Total exercises1 = {part1.exercises} 
-        exercises2 = {part2.exercises}
-        exercises3 = {part3.exercises}
+      <Total exercises1 = {parts[0].exercises} 
+        exercises2 = {parts[1].exercises}
+        exercises3 = {parts[2].exercises}
       />            
     </div>
   )
