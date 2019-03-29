7352b16b2d1c4536f30d31779e8582b2caa6edcb
Dmitri <34885758+Dmitri9149@users.noreply.github.com>
2019-03-29 16:30:16 +0200

    1.4. modified see HUOM: to teh task

--- kurssitiedot/src/index.js
+++ kurssitiedot/src/index.js
@@ -22,16 +22,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content 
-        part1 = {parts[0].name}  exercises1 = {parts[0].exercises}
-        part2 = {parts[1].name}  exercises2 = {parts[1].exercises}
-        part3 = {parts[2].name}  exercises3 = {parts[2].exercises}
-       />
-      
-      <Total exercises1 = {parts[0].exercises} 
-        exercises2 = {parts[1].exercises}
-        exercises3 = {parts[2].exercises}
-      />            
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
@@ -52,7 +44,8 @@ const Total = (props)=> {
   return (
     <div>
       <p>
-      yhteensä {props.exercises1 + props.exercises2 + props.exercises3} tehtävää
+        yhteensä {props.parts[0].exercises + 
+        props.parts[1].exercises + props.parts[2].exercises} tehtävää
       </p>
     </div>
   )
@@ -62,7 +55,7 @@ const Part = (props)=> {
   return (
     <div>
       <p>
-        {props.part} {props.exercises}
+        {props.part.name} {props.part.exercises}
       </p>
     </div>  
   )
@@ -71,9 +64,9 @@ const Part = (props)=> {
 const Content = (props)=> {
   return (
     <div>
-      <Part part ={props.part1} exercises = {props.exercises1}/>
-      <Part part ={props.part2} exercises = {props.exercises2}/>
-      <Part part ={props.part3} exercises = {props.exercises3}/>
+      <Part part ={props.parts[0]} exercises = {props.parts[0]}/>
+      <Part part ={props.parts[1]} exercises = {props.parts[1]}/>
+      <Part part ={props.parts[2]} exercises = {props.parts[2]}/>
       
     </div>
   )
