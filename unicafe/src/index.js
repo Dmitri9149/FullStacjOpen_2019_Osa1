9fd8dc33a6843da2d2cc4e3394cc4367fba0c655
Dmitri <34885758+Dmitri9149@users.noreply.github.com>
2019-03-30 20:50:41 +0200

    unicafe

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -6,6 +6,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  
 
   return (
     <div>
@@ -14,10 +15,10 @@ const App = () => {
         <button onClick={() => setGood(good + 1)}>
           good
         </button>
-        <button onClick={() => setNeutral(neutral + 1)}>
+        <button onClick={()=>setNeutral(neutral+1)}>
           neutral
         </button>
-        <button onClick={() => setBad(bad + 1)}>
+        <button onClick={()=>setBad(bad +1)}>
           bad
         </button>
 
@@ -32,10 +33,6 @@ const App = () => {
 }
 
 const Heading = ({nimi}) => <h1>{nimi}</h1> 
-
-
-
-  
 
 ReactDOM.render(<App />, 
   document.getElementById('root')
